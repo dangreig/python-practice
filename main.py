1878f4194a9f25ef3f689bd9cda4b8a475433a4f
dangreig <dangreig96@gmail.com>
2026-04-15 20:19:56 +0100

    updated calc

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 print("Tiny Calculator")
-print("Operations: +, -, *, /")
 
-operation = input("Choose operation: ")
-num1 = float(input("First number: "))
-num2 = float(input("Second number: "))
+while True:
+    print("\nOperations: +, -, *, /")
+    operation = input("Choose operation: ")
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
 
-if operation == "+":
-    result = num1 + num2
-    print("Result:", result)
-elif operation == "-":
-    result = num1 - num2
-    print("Result:", result)
-elif operation == "*":
-    result = num1 * num2
-    print("Result:", result)
-elif operation == "/":
-    if num2 == 0:
-        print("You cannot divide by zero.")
-    else:
-        result = num1 / num2
+    if operation == "+":
+        result = num1 + num2
         print("Result:", result)
-else:
-    print("Invalid operation")
+    elif operation == "-":
+        result = num1 - num2
+        print("Result:", result)
+    elif operation == "*":
+        result = num1 * num2
+        print("Result:", result)
+    elif operation == "/":
+        if num2 == 0:
+            print("You cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print("Result:", result)
+    else:
+        print("Invalid operation")
+
+    again = input("Do you want another calculation? (yes/no): ").lower()
+    if again != "yes":
+        print("Goodbye.")
+        break
